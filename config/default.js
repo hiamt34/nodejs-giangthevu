c2956053232ad4da0f7eaa3e34cdca112e38c7d3
hiiamt34 <d17dccn195@gmail.com>
2021-12-23 00:30:42 +0700

    update deploy

--- config/default.js
+++ config/default.js
@@ -1,9 +1,10 @@
 module.exports = {
+    // dbUri: 'mongodb+srv://truongphi:truongphi23@cluster0.wxbhm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     dbUri: 'mongodb+srv://truongphi:truongphi23@cluster0.wxbhm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 
     "vnp_TmnCode":"ODO5U169",
     "vnp_HashSecret":"WLDIFVOJZZFLOIHGPBKIAOZUIVGVCEZP",
     "vnp_Url": "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html",
-    "vnp_ReturnUrl": "http://localhost:3000/order/vnpay_return",
+    "vnp_ReturnUrl": "https://giangthevuvnpay.herokuapp.com/order/vnpay_return",
     "redirec": "http://localhost:3001/home",
 }
